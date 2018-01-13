b28c29a183c11d759edcb8451fdc247cefb258d3
= <jerocc002@gmail.com>
2018-01-13 21:59:21 +0100

    VIS_HTML_CSS_JS cleaning

--- VIS_HTML_CSS_JS/codigo.js
+++ VIS_HTML_CSS_JS/codigo.js
@@ -18,22 +18,7 @@ function openTab(evt, tabName) {
 var map = L.map('map', {attributionControl:false}).setView([37.38, -5.98], 12);
 
 // Capa base
-/*
-L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-}).addTo(map);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	id: 'mapbox.streets-satellite' //run-bike-hike streets-satellite streets
-}).addTo(map);
-*/
-
 L.esri.basemapLayer("Topographic").addTo(map);
-//L.esri.basemapLayer('Streets').addTo(map);
 
 // Marcador coloreado según gradiente para capa estación
 // por la propiedad stands
